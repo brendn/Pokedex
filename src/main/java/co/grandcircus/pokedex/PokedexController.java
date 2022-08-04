d75bd3a78b7d37b75c9d7a06b28aaea1095679dd
Megan Coats <ctsmgn@yahoo.com>
2022-08-04 12:51:48 -0400

    Update to search features

--- src/main/java/co/grandcircus/pokedex/PokedexController.java
+++ src/main/java/co/grandcircus/pokedex/PokedexController.java
@@ -39,9 +39,34 @@ public class PokedexController {
 
     @PostMapping("/searchresults")
     public String search(@RequestParam String search, Model model) {
-        model.addAttribute("search", service.getPokemonByName(search));
-        model.addAttribute("pokemon", service.getPokemon(151));
-        return "searchresults";
+    	search = search.toLowerCase();
+    	Pokemon searchPokemon = service.getPokemonInfo(service.getPokemonByName(search).getID());
+    	try {
+			if (!(search.equals("")) && !(service.getPokemonByName(search) == null)) {
+				StringBuilder types = new StringBuilder("[");
+
+		        for (int i = 0; i < searchPokemon.getTypes().length; i++) {
+		            if (i == searchPokemon.getTypes().length - 1) {
+		                types.append(searchPokemon.getTypes()[i]);
+		            } else {
+		                types.append(searchPokemon.getTypes()[i]).append(", ");
+		            }
+		        }
+		        types.append("]");
+		        model.addAttribute("types", types.toString());
+				model.addAttribute("search", service.getPokemonByName(search));
+				model.addAttribute("searchPokemon", service.getPokemonInfo(service.getPokemonByName(search).getID()));
+				
+				return "searchresults";
+			} else {
+				
+				return "redirect:/";
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "redirect:/";
+		}
     }
 
 }
--- src/main/java/co/grandcircus/pokedex/controllers/SearchResultsController.java
+++ src/main/java/co/grandcircus/pokedex/controllers/SearchResultsController.java
@@ -21,33 +21,48 @@ public class SearchResultsController {
 
 	@Autowired
 	public PokemonService service;
-	
+
 	@Autowired
 	private PokemonRepository repo;
 
 	@RequestMapping("/searchresults")
-	public String searchResults(Model model, @ModelAttribute("search") String search) {
-		model.addAttribute("pokemon", service.getPokemon(151));
-		// model.addAttribute("pokemon",service.getPokemonInfo(5));
+	public String searchResults(Model model, @ModelAttribute("Search") String search) {
+		search = search.toLowerCase();
+    	
+    	try {
+			if (!(search.equals("")) && !(service.getPokemonByName(search) == null)) {
+				Pokemon searchPokemon = service.getPokemonInfo(service.getPokemonByName(search).getID());
+				StringBuilder types = new StringBuilder("[");
 
-		if (search != null) {
-
-			model.addAttribute("pokemonChoice", service.getPokemonByName(search));
-			return "searchResults";
-		} else {
-			model.addAttribute("error", "Invalid Details");
-			return "home";
+		        for (int i = 0; i < searchPokemon.getTypes().length; i++) {
+		            if (i == searchPokemon.getTypes().length - 1) {
+		                types.append(searchPokemon.getTypes()[i]);
+		            } else {
+		                types.append(searchPokemon.getTypes()[i]).append(", ");
+		            }
+		        }
+		        types.append("]");
+		        model.addAttribute("types", types.toString());
+				model.addAttribute("search", service.getPokemonByName(search));
+				model.addAttribute("searchPokemon", searchPokemon);
+				
+				return "searchresults";
+			} else {
+				
+				return "redirect:searchresults";
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "redirect:searchresults";
 		}
-	}
-
-	
-	
+    }
 
 	@PostMapping("/addfavorite")
 	public String addFavorite(@RequestParam String favorite, Model model) {
 		FavoritePokemon pokemon = new FavoritePokemon(service.getPokemonByName(favorite).getID(),
 				service.getPokemonByName(favorite).getName());
-		
+
 		repo.save(pokemon);
 		return "redirect:/";
 	}
